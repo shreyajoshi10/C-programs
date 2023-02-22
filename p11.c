19017c772ba6d0228237f08b607d946e1e0d826a
shreya joshi <shreyajoshi10.04@gmail.com>
2023-02-22 16:36:47 +0530

    Adding array elements

---
+++ p11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    int n, i;
+    int a[n];
+    printf("Enter the number of elements you want to add in the array ");
+    scanf("%d", &n);
+    for(i=0; i<n; i++)
+    {
+        printf("Enter the %d element ", i+1);
+        scanf("%d", &a[i]);
+    }
+    for i in a[n]
+    {
+        printf(a[i])
+    }
+
+}
